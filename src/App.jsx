d7dcc52096af1ed92cec436eb92c57c286bc0760
Parham-Kera <157530574+Parham-Kera@users.noreply.github.com>
2024-03-14 15:18:04 +0000

    fixing errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./login";
 
 export default function App() {
   return (
-    <main>
-      React ⚛️ + Vite ⚡ + Replit
-    </main>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
